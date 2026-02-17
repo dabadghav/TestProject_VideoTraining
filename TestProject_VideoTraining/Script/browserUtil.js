4a860499e39741d8a172313b7001d0a8fa582310
dabadghav <sdabadghav@planittesting.com>
2026-02-17 22:01:04 +1100

    Initial commit with .gitignore to exclude unnecessary files

---
+++ TestProject_VideoTraining/Script/browserUtil.js
@@ -0,0 +1,31 @@
+﻿//JScript
+function runIncognitoMode(browserName){
+  //var browserName = "firefox" //iexplore,edge,chrome,firefox
+  if (Sys.WaitBrowser(browserName).Exists){
+    var browser = Sys.Browser(browserName);
+    Log.Enabled = false // To disable the warning that might occur during closing of the browser
+    browser.Close();
+    Log.Enabled = true // enabling the logs back
+  }
+  if(browserName=="edge"){
+    Browsers.Item(btEdge).RunOptions = "-inprivate"
+    Delay(3000)
+    Browsers.Item(btEdge).Run();
+  }
+  else if (browserName=="iexplore"){
+    Browsers.Item(btIExplorer).RunOptions = "-private"
+    Delay(3000)
+    Browsers.Item(btIExplorer).Run();
+  }
+  else if (browserName=="chrome"){
+    Browsers.Item(btChrome).RunOptions = "-incognito"
+    Delay(3000)
+    Browsers.Item(btChrome).Run();
+  }
+  else if (browserName=="firefox"){
+    Browsers.Item(btFirefox).RunOptions = "-private"
+    Delay(3000)
+    Browsers.Item(btFirefox).Run();
+  }
+  Sys.Browser(browserName).BrowserWindow(0).Maximize()
+}
---
+++ TestProject_VideoTraining/Script/mathUtil.js
@@ -0,0 +1,12 @@
+﻿//JavaScript
+//JScript
+
+function test()
+{
+ Log.Message("Generated random number: " + getRandomNumberBetween(30,75))
+}
+
+function getRandomNumberBetween(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
